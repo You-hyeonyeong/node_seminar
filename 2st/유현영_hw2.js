c14879ada6441fda092909d6f3a4dca055f48de0
You-hyeonyeong <dbgusdud412@naver.com>
2019-04-29 23:50:13 +0900

    2st homework com

--- 2st/유현영_hw2.js
+++ 2st/유현영_hw2.js
@@ -23,60 +23,61 @@ const server = http.createServer((req, res) => {
     const infoOption = {
         uri: "http://15.164.75.18:3000/homework/2nd",
         method: "POST",
-        body : {
-            'name' : '유현영',
-            'phone' : '010-5006-0804'
+        body: {
+            'name': '유현영',
+            'phone': '010-5006-0804'
         },
-        json : true
+        json: true
     };
-    
+
     let keys = {
         "id": "",
         "hashedPw": "",
         "salt": ""
     };
     let data = {
-        "msg" : ""
+        "msg": ""
     }
 
-     const urlParse = url.parse(req.url);
+    const urlParse = url.parse(req.url);
 
-     if(urlParse.pathname == "/signup"){
+    //signup 회원가입
+    if (urlParse.pathname == "/signup") {
         const queryParse = queryString.parse(urlParse.query);
         const id = queryParse.id;
         const pw = queryParse.pw;
-   
-        cryptoModule(pw,function(result){
-           //id값, pw값, salt값 csv에 저장
-           keys.id = id
-           keys.hashedPw = result.hashedPassword
-           keys.salt = result.salt
-   
-           const keysCsv = json2csv.parse(keys)
-   
-           fs.writeFile('info.csv', keysCsv, (err) => {
-               if (err) {
-                   data.msg = "file save err";
-               } else {
-                   console.log("All of complete!");
-                   res.write("signup success");
-                   res.end();
-               }
-           })
-   
-       });
-       //signin 
-     } else if (urlParse.pathname == "/signin"){
-         
-         const queryParse = queryString.parse(urlParse.query);
-         const id = queryParse.id;
-         const pw = queryParse.pw;
 
-         csv2json().fromFile('./info.csv').then((data) => {
+        cryptoModule(pw, function (result) {
+            //id값, pw값, salt값 csv에 저장
+            keys.id = id
+            keys.hashedPw = result.hashedPassword
+            keys.salt = result.salt
+
+            const keysCsv = json2csv.parse(keys)
+
+            fs.writeFile('info.csv', keysCsv, (err) => {
+                if (err) {
+                    data.msg = "file save err";
+                } else {
+                    console.log("All of complete!");
+                    res.write("signup success");
+                    res.end();
+                }
+            })
+
+        });
+        //signin 로그인
+    } else if (urlParse.pathname == "/signin") {
+
+        const queryParse = queryString.parse(urlParse.query);
+        const id = queryParse.id;
+        const pw = queryParse.pw;
+
+        csv2json().fromFile('./info.csv').then((data) => {
             console.log(data)
-            cryptoModule.encryptionWithSalt(pw,data[0].salt,function(result){
+            cryptoModule.encryptionWithSalt(pw, data[0].salt, function (result) {
                 console.log(result)
-                if(result.hashedPassword == data[0].hashedPw){
+                if (result.hashedPassword == data[0].hashedPw) {
                     res.write("login success")
                     res.end()
                 } else {
@@ -86,20 +87,37 @@ const server = http.createServer((req, res) => {
             })
         })
 
-        //  fs.readFile('./info.csv',{encoding:"utf-8"},function(error,data){
-        //     console.log(data);
-        
-        // });
-        
-     } else {
-        
+        //info 
+    } else {
         request(infoOption, (err, response, key) => {
-            console.log(response)
+            if (err) {
+                console.log(err)
+            } else {
+                if (response.body.status == 400) {
+                    res.write("no user")
+                    res.end()
+                } else if (response.body.status == 200) {
+                    console.log(response.body)
+                    res.write("user search complete")
+                    const profileCsv = json2csv.parse(response.body.data)
+                    
+                    fs.writeFile('profile.csv', profileCsv, (err) => {
+                        if (err) {
+                            data.msg = "file save err";
+                        } else {
+                            console.log("All of complete!");
+                            res.write("profile save");
+                            res.end()
+                        }
+                    })
+                } else {
+                    res.write("뭐야 넌!!!!!!!!!!!")
+                    res.end()
+                }
+            }
 
         })
-         
-     }
-     
+    }
 }).listen(3000, (req, res) => {
     console.log("port connected!")
 })
